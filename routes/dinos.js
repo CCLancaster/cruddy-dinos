76c370a1e2f69f45b84c30f502bd4ce5d810e97d
CCLancaster <connieclancaster@gmail.com>
2020-02-12 14:54:53 -0800

    stubbed out other routes

--- routes/dinos.js
+++ routes/dinos.js
@@ -1,12 +1,19 @@
 //mounted at '/dinos'
 
+//this are all my routes...
+
+
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 //INDEX -GET
 router.get('/', (req, res) => {
-//TODO get all dinos, pass to page
-    res.render('dinos/index', { dinos: [] });
+//get all dinos, pass to page
+    let allDinos = fs.readFileSync('./dinosaurs.json');
+    let dinoData = JSON.parse(allDinos);
+
+    res.render('dinos/index', { dinos: dinoData });
 });
 
 //NEW - GET
@@ -15,23 +22,78 @@ router.get('/new', (req, res) => {
 });
 
 //CREATE - POST
+router.post('/', (req, res) => {
+    console.log(req.body);
+    // read dinos
+    let dinos = fs.readFileSync('./dinosaurs.json');
+    // JSON parse dinos
+    let dinoData = JSON.parse(dinos);
+    // add req.body to the end of dinos
+    dinoData.push(req.body);
+    // JSON stringify dinos
+    let newDinos = JSON.stringify(dinoData);
+    // write dinos
+    fs.writeFileSync('./dinosaurs.json', newDinos);
 
+    //TODO redirect to show page for new dino
+    res.redirect(`/dinos/${dinoData.length - 1}`);
+})
 
 //SHOW - GET
 router.get('/:id', (req, res) => {
-//TODO Get actual Dino
+//Get actual dino
+    let dinos = fs.readFileSync('./dinosaurs.json');
+    let dinoData = JSON.parse(dinos);
+    let dinoIndex = parseInt(req.params.id);
+    let oneDino = dinoData[dinoIndex];
+    oneDino.id = dinoIndex;
 
-    res.render('/dinos/show', { dino: { id: req.params.id }});
+    res.render('dinos/show', { dino: oneDino });
 });
 
 //EDIT - GET
 router.get('/edit/:id', (req, res) => {
 //TODO get actual dino
-    res.render('/dinos/edit', { dino: { id: req.params.id }});
+    let dinos = fs.readFileSync('./dinosaurs.json');
+    dinos = JSON.parse(dinos);
+    let dinoIndex = parseInt(req.params.id);
+    let oneDino = dinos[dinoIndex];
+    oneDino.id = dinoIndex;
+
+    res.render('dinos/edit', { dino: oneDino });
 });
 
 //UPDATE - PUT
+router.put('/:id', (req, res) => {
+   console.log(req.body);
+    // read the file
+    let dinos = fs.readFileSync('./dinosaurs.json');
+    // json parse the dinos
+    dinos = JSON.parse(dinos);
+    //change thename and type of dino at index
+    dinos[parseInt(req.params.id)] = req.body
+   
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos));
+    // redirect
+    res.redirect(`/dinos/${req.params.id}`);
+});
 
 //DESTROY - DELETE
+router.delete('/:id', (req, res) => {
+    // console.log(`Deleting dino at id ${req.params.id}`);
+
+    // read dinos
+    let dinos = fs.readFileSync('./dinosaurs.json');
+    //JSON parse dinos
+    dinos = JSON.parse(dinos);
+    // remove dino from array at index
+        //splice of life - it returns a value
+    let deadDino = dinos.splice(req.params.id, 1);
+
+    // write JSON stringified version of dinos
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos));
+    console.log(`Press F to pay respects to ${deadDino[0].name}`);
+    res.redirect('/dinos');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 //IMPORT
 const express = require('express');
 const layouts = require('express-ejs-layouts')
+const methodOverride = require('method-override');
 
 // create instance of express
 const app = express();
 // giving the app some settings
 app.set('view engine', 'ejs');
 app.use(layouts);
+app.use(express.urlencoded({ extended: false }));
+//this method override needs to come BEFORE the controler below that shows all the routes
+app.use(methodOverride('_method'));
 
 // SUM ROUTES HERE
 app.get('/', (req, res) => {
